9ef00ccf5f9e410b76d468e0f1b8d3e47ca3f7a1
Aszmel <mellipone@gmail.com>
2019-08-07 18:13:26 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Homepage from "./Homepage";
+
+function App() {
+  return (
+    <div>
+      <h3>Here goes menu</h3>
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
