d7f2105a0abaf90799102f2e41e0bbc0b319dfbb
Aszmel <mellipone@gmail.com>
2019-08-12 18:18:19 +0100

    add Header, styled it and set on all pages

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import Homepage from "./pages/homepage/Homepage";
 import Shop from "./pages/shop/Shop";
+import Header from "../src/components/header/Header";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/shop" component={Shop} />
