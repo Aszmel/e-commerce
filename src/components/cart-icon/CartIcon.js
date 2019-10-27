79e4bf02859906ebd5659f1c588177d022300382
Aszmel <mellipone@gmail.com>
2019-10-27 18:31:21 +0100

    added reselect library, replaced cart selectors with memoized selectors to not update state every time when cart items ie quantity change

--- src/components/cart-icon/CartIcon.js
+++ src/components/cart-icon/CartIcon.js
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
+import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
 import "./CartIcon.scss";
 
 const CartIcon = ({ toggleCartHidden, itemCount }) => {
@@ -17,11 +18,8 @@ const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  itemCount: cartItems.reduce(
-    (accuQuantity, cartItem) => accuQuantity + cartItem.quantity,
-    0
-  )
+const mapStateToProps = state => ({
+  itemCount: selectCartItemsCount(state)
 });
 
 export default connect(
