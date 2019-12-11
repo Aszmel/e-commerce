b800d4392d57f99e6f314cb15c6d9c6e18627030
Aszmel <mellipone@gmail.com>
2019-12-11 17:47:32 +0100

    created CollectionsOverview component to make it render whole collections, then going to add routing to make it possible to go to each category from this component

--- src/pages/shop/Shop.js
+++ src/pages/shop/Shop.js
@@ -1,19 +1,9 @@
 import React from "react";
-import { connect } from "react-redux";
-import PreviewCollection from "../../components/preview-collection/PreviewCollection";
-import { selectCollections } from "../../redux/shop/shop.selector";
-import { createStructuredSelector } from "reselect";
-
-const Shop = ({ collections }) => (
+import CollectionsOverview from "../../components/collections-overwiew/CollectionsOverview";
+const Shop = () => (
   <div className="shop-page">
-    {collections.map(({ id, ...otherCollectionProps }) => (
-      <PreviewCollection key={id} {...otherCollectionProps} />
-    ))}
+    <CollectionsOverview />
   </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollections
-});
-
-export default connect(mapStateToProps)(Shop);
+export default Shop;
