9200bdf1d811a37cd186c161b248043c285669d4
Aszmel <mellipone@gmail.com>
2019-12-26 21:06:30 +0100

    renaming component to better descriptive meaning

---
+++ src/pages/collection/Collection.js
@@ -0,0 +1,11 @@
+import React from "react";
+import CollectionItem from "../../components/collection-item/CollectionItem";
+import "./Collection.scss";
+
+const Collection = ({ match }) => (
+  <div className="collection">
+    <h2>collection page</h2>
+  </div>
+);
+
+export default Collection;
--- src/pages/shop/Shop.js
+++ src/pages/shop/Shop.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import CollectionsOverview from "../../components/collections-overwiew/CollectionsOverview";
-import Category from "../category/Category";
+import Collection from "../collection/Collection";
 
 const Shop = ({ match }) => (
   <div className="shop-page">
     <Route exact path={`${match.path}`} component={CollectionsOverview} />
-    <Route path={`${match.path}/:categoryId`} component={Category} />
+    <Route path={`${match.path}/:categoryId`} component={Collection} />
   </div>
 );
 
