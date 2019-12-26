9b86efd195462b122ead97c5a512f8cb57f41b47
Aszmel <mellipone@gmail.com>
2019-12-26 21:43:29 +0100

    finished dynamic routing on Collection component

--- src/pages/collection/Collection.js
+++ src/pages/collection/Collection.js
@@ -1,11 +1,18 @@
 import React from "react";
-import CollectionItem from "../../components/collection-item/CollectionItem";
+import { connect } from "react-redux";
+import { selectCollection } from "../../redux/shop/shop.selector";
 import "./Collection.scss";
 
-const Collection = ({ match }) => (
-  <div className="collection">
-    <h2>collection page</h2>
-  </div>
-);
+const Collection = ({ collection }) => {
+  return (
+    <div className="collection">
+      <h2>collection page</h2>
+    </div>
+  );
+};
 
-export default Collection;
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
+
+export default connect(mapStateToProps)(Collection);
