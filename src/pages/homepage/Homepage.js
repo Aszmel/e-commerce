652e96987c62c7009e82df6097411438ed327a79
Aszmel <mellipone@gmail.com>
2020-08-20 19:29:25 +0100

    adding styled components to Header and Homepage

--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Directory from "../../components/directory/Directory";
-import "./Homepage.scss";
+// import "./Homepage.scss";
+import { HomepageContainer } from "./homepage.styles";
 
 const Homepage = () => {
   return (
-    <div className="homepage">
+    <HomepageContainer>
       <Directory />
-    </div>
+    </HomepageContainer>
   );
 };
 
